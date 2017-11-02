356fb2f53d8c7c464afe32db415eb4650c94aa81
God Tariq <tariq@ramesar.net>
2017-11-02 19:51:47 -0400

    Zedd

--- SignatureScript.js
+++ SignatureScript.js
@@ -1,19 +1,16 @@
 // How to test in Chrome's console: https://stackoverflow.com/questions/5282228/include-javascript-file-in-chrome-console
 /* Ctrl + Shift + J on the Chrome Page -> Console tab
-var script = document.createElement('script');
-script.type = 'application/javascript';
+var script = document.createElement('script').type = 'application/javascript';
 script.src = 'https://rawgit.com/Ramlall/Scripts/master/SignatureScript.js';
 document.head.appendChild(script);
-
-
 */
 
 /* Create a function for when a user submits for the signature form. */
 console.log("OB3LISK successfully loaded the signature script.");
 // When the "Save Signature" button gets clicked.
-$("form [name=vbform]").on("submit", function(event)
+$("[name=vbform]").on("submit", function(event)
 	{
 	// Don't reload the page.
 	event.preventDefault();
-	console.log("Sup mofo.");
+	console.log("I want you to know that I'm all yours. You and me...we're the same force.");
 	});
