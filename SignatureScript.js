d710549243ff91b7ec07572e43ddd9395c7f9411
God Tariq <tariq@ramesar.net>
2017-11-02 19:54:30 -0400

    Starboy

--- SignatureScript.js
+++ SignatureScript.js
@@ -1,6 +1,11 @@
 // How to test in Chrome's console: https://stackoverflow.com/questions/5282228/include-javascript-file-in-chrome-console
 /* Ctrl + Shift + J on the Chrome Page -> Console tab
-var script = document.createElement('script').type = 'application/javascript';
+var script = document.createElement('script');
+script.type = 'application/javascript';
+script.src = 'https://rawgit.com/Ramlall/Scripts/master/SignatureScript.js';
+document.head.appendChild(script);
+
+var script = document.createElement('script').type('application/javascript');
 script.src = 'https://rawgit.com/Ramlall/Scripts/master/SignatureScript.js';
 document.head.appendChild(script);
 */
