08f6dd381009a798b4e482f2c5ef4d9c561e5946
God Tariq <tariq@ramesar.net>
2017-11-02 18:48:28 -0400

    Selena Gomez

--- SignatureScript.js
+++ SignatureScript.js
@@ -1,3 +1,14 @@
+// How to test in Chrome's console: https://stackoverflow.com/questions/5282228/include-javascript-file-in-chrome-console
+/* Ctrl + Shift + J on the Chrome Page -> Console tab
+var script = document.createElement('script');
+script.type = 'application/javascript';
+script.src = 'https://github.com/Ramlall/Scripts/blob/master/SignatureScript.js';
+document.head.appendChild(script);
+
+
+*/
+
+<script>
 /* Create a function for when a user submits for the signature form. */
 console.log(OB3LISK);
 // When the "Save Signature" button gets clicked.
@@ -7,3 +18,4 @@ $("form [name=vbform]").on("submit", function(event)
 	event.preventDefault();
 	console.log("Sup mofo.");
 	});
+</script>
