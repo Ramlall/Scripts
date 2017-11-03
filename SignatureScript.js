1051c359d87f21c6d98e4dcdfcee4303f0c551ca
God Tariq <tariq@ramesar.net>
2017-11-03 00:38:40 -0400

    Added break condition for ending with [IMG]

--- SignatureScript.js
+++ SignatureScript.js
@@ -1,13 +1,14 @@
 // How to test in Chrome's console: https://stackoverflow.com/questions/5282228/include-javascript-file-in-chrome-console
 /* Ctrl + Shift + J on the Chrome Page -> Console tab
-var script = document.createElement('script');	// Create the script.
-script.type = 'application/javascript';			// Make sure the type is correct. 
-script.src = ''; 								// Add your script url. Use rawgit to convert a git file to the correct headers (or else you get MIME errors).
-document.head.appendChild(script);				// Add the script to the HTML head.
+var script = document.createElement('script');	
+script.type = 'application/javascript';			
+script.src = ''; 								
+document.head.appendChild(script);				
 */
 
+console.log("Successfully loaded the signature script.");
+
 /* Create a function for when a user submits for the signature form. */
-console.log("OB3LISK successfully loaded the signature script.");
 // When the "Save Signature" or "Preview" button gets clicked.button gets clicked.
 $("[name=vbform]").on("submit", function(event)
 	{
@@ -16,26 +17,24 @@ $("[name=vbform]").on("submit", function(event)
 	// Don't reload the page until we check the image dimensions.
 	event.preventDefault();
 	
-	// Get the text for the signature being submitted into a string variable.
+	// Get the raw text for the signature being submitted.
 	var sigtext = $(this).find("#vB_Editor_001_editor").val();
-	//console.log(sigtext);
-	// WORKS
 	
 	// Search that string for any images being rendered.
-	// Check each char for [IMG]
+	// Look for "[IMG]" tags
 	for(let i = 0; i < sigtext.length - 5; i++) // Each character that could be an [ (From "[IMG]").
 		{
 		// Get this and the next 4 chars.
 		var nextfive = sigtext[i+0] + sigtext[i+1] + sigtext[i+2] + sigtext[i+3] + sigtext[i+4];
 		
 		// If it's not "[IMG]" then move to the next char.
-		if(nextfive != "[IMG]")
+		if((nextfive != "[IMG]") || i+5 == sixtext.length) // Extra condition: if a user ends their signature with [IMG] for some reason, there's no url to parse.
 			{ continue; }
 		
-		// We found an image tag! Reposition i to be the first character of the image url.
-		i = i+5; 
-		var url = "";
-		for(let j = i; j < sigtext.length - 5; j++)
+		// We found an image tag! Reposition i to be the first character after [ in "[IMG]" 
+		i = i+5;
+		var url = "";	// Hold our url in this string.
+		for(let j = i; j < sigtext.length - 5; j++)	// Check every character after the [IMG] tag until we find [/IMG] or reach the end of the characters.
 			{
 			// If we find the "[/IMG]" tag....
 			if(sigtext[j+0] === '[' && sigtext[j+1] === '/' && sigtext[j+2] === 'I' && sigtext[j+3] === 'M' && sigtext[j+4] === 'G' && sigtext[j+5] === ']') 
@@ -53,7 +52,6 @@ $("[name=vbform]").on("submit", function(event)
 			// This is a valid url char, so add it to the string.
 			url += sigtext[j];
 			}
-	
 		}
 	});
 	
