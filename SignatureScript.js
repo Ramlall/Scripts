a0921d4a622c4b5dfddc73f8cab07b4fe88e3072
God Tariq <tariq@ramesar.net>
2017-11-02 23:36:23 -0400

    .

--- SignatureScript.js
+++ SignatureScript.js
@@ -25,11 +25,12 @@ $("[name=vbform]").on("submit", function(event)
 	// Check each char for [IMG]
 	for(let i = 0; i < sigtext.length - 5; i++) // Each character that could be an [ (From "[IMG]").
 		{
-		if(sigtext[i+0] != '[') { continue; }
-		if(sigtext[i+1] != 'I') { continue; }
-		if(sigtext[i+2] != 'M') { continue; }
-		if(sigtext[i+3] != 'G') { continue; }
-		if(sigtext[i+4] != ']') { continue; }
+		// Get this and the next 4 chars.
+		var nextfive = sigtext[i+0] + sigtext[i+1] + sigtext[i+2] + sigtext[i+3] + sigtext[i+4];
+		
+		// If it's not "[IMG]" then move to the next char.
+		if(nextfive != "[IMG]")
+			{ continue; }
 		
 		// We found an image tag! Reposition i to be the first character of the image url.
 		i = i+5; 
@@ -39,13 +40,13 @@ $("[name=vbform]").on("submit", function(event)
 			// If we find the "[/IMG]" tag....
 			if(sigtext[j+0] === '[' && sigtext[j+1] === '/' && sigtext[j+2] === 'I' && sigtext[j+3] === 'M' && sigtext[j+4] === 'G' && sigtext[j+5] === ']') 
 				{
-				// Stop checking this url. 
-				
-				// Advance i to the last bracket.
+				// Advance i to to that final bracket in "[/IMG]".
 				i = j+5;
 				
 				// Test the validity of this image's dimensions.
 				CheckImage(url);
+				
+				// Stop checking this url. 
 				break;
 				}
 			
@@ -54,16 +55,11 @@ $("[name=vbform]").on("submit", function(event)
 			}
 	
 		}
-	
-	// For each image...
-		// Render each image and get the height and width.
-		// If the height or width is above the required size...
-		// Prevent the page from reloading.
-		// Add an error message saying "You have an image above the allowed dimensions."
 	});
 	
 // Function to process an image url.
 function CheckImage(imageurl)
 	{
 	console.log(imageurl);
+	// Get the dimensions of this image.
 	}
